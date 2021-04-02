bedca6aea601f5e0905deb7597955e71c28c046c
PDGHACK-10 <81812004+PDGHACK-10@users.noreply.github.com>
2021-04-02 20:45:31 +0800

    Create termux-all-package.sh

---
+++ termux-all-package.sh
@@ -0,0 +1,75 @@
+#!/bin/bash
+## termux-all-pkg v4.1
+## coded by: https://github.com/PRINCEGUT-12
+## run the script without error in termux to install all termux-all-package
+
+printf "\e[1;40m CODED BY:{PRINCE GUTIERREZ} {PRINCEGUT-12}\e[0m\n"
+printf "\e[1;40m ████████╗███████╗██████╗░███╗░░░███╗██╗░░░██╗██╗░░██╗\e[0m\n"
+printf "\e[1;40m ╚══██╔══╝██╔════╝██╔══██╗████╗░████║██║░░░██║╚██╗██╔╝\e[0m\n"
+printf "\e[1;40m ░░░██║░░░█████╗░░██████╔╝██╔████╔██║██║░░░██║░╚███╔╝░\e[0m\n"
+printf "\e[1;40m ░░░██║░░░██╔══╝░░██╔══██╗██║╚██╔╝██║██║░░░██║░██╔██╗░\e[0m\n"
+printf "\e[1;40m ░░░██║░░░███████╗██║░░██║██║░╚═╝░██║╚██████╔╝██╔╝╚██╗\e[0m\n"
+printf "\e[1;40m ░░░╚═╝░░░╚══════╝╚═╝░░╚═╝╚═╝░░░░░╚═╝░╚═════╝░╚═╝░░╚═╝\e[0m\n"
+printf "\e[1;40m                 ░█████╗░██╗░░░░░██╗░░░░░\e[0m\n"
+printf "\e[1;40m                 ██╔══██╗██║░░░░░██║░░░░░\e[0m\n"
+printf "\e[1;40m                 ███████║██║░░░░░██║░░░░░\e[0m\n"
+printf "\e[1;40m                 ██╔══██║██║░░░░░██║░░░░░\e[0m\n"
+printf "\e[1;40m                 ██║░░██║███████╗███████╗\e[0m\n"
+printf "\e[1;40m                 ╚═╝░░╚═╝╚══════╝╚══════╝\e[0m\n"
+printf "\e[1;40m ██████╗░░█████╗░░█████╗░██╗░░██╗░█████╗░░██████╗░ ███████╗\e[0m\n"
+printf "\e[1;40m ██╔══██╗██╔══██╗██╔══██╗██║░██╔╝██╔══██╗██╔════╝░ ██╔════╝\e[0m\n"
+printf "\e[1;40m ██████╔╝███████║██║░░╚═╝█████═╝░███████║██║░░██╗░ █████╗░░\e[0m\n"
+printf "\e[1;40m ██╔═══╝░██╔══██║██║░░██╗██╔═██╗░██╔══██║██║░░╚██╗ ██╔══╝░░\e[0m\n"
+printf "\e[1;40m ██║░░░░░██║░░██║╚█████╔╝██║░╚██╗██║░░██║╚██████╔╝ ███████╗\e[0m\n"
+printf "\e[1;40m ╚═╝░░░░░╚═╝░░╚═╝░╚════╝░╚═╝░░╚═╝╚═╝░░╚═╝░╚═════╝░ ╚══════╝v4.1\e[0m\n"
+sleep 1
+apt upgrade -y
+apt upgrade  -y
+apt install git  -y
+apt install python  -y
+apt install python2  -y
+apt install toilet  -y
+apt install wget  -y
+apt install figlet  -y
+apt install php  -y
+apt install libcaca  -y
+apt install ruby  -y
+pip install lolcat  -y
+apt install mechanize -y
+pkg install ncurses-utils -y
+pkg install curl -y
+pkg install openssh -y
+gem install lolcat  -y
+apt install root-repo -y 
+apt install x11-repo -y
+apt install unzip -y
+apt install tor -y
+apt install zip -y
+apt install nano -y
+apt install curl -y
+apt install bash -y
+apt install clang -y
+apt install wireguard-tools  -y
+apt install bmon -y
+apt install perl -y
+pip install unstable-repo
+apt install nmap -y
+apt install proot  -y
+pip install requestes  -y
+apt install perl  -y
+apt install clang  -y
+apt install tmux  -y
+pkg install cowsay  -y
+apt install neofetch   -y
+apt install cmatrix  -y
+apt install sl  -y
+pkg install net-tools  -y
+pkg install grep  -y
+pkg install termshark  -y
+termux-setup-storage 
+apt install termux-api -y
+pkg install science-repo -y
+pkg install game-repo -y
+pkg install fakeroot -y
+pkg install aircrack-ng -y
+echo -e $blue TERMUX PACKAGE SUCCESSFULLY✓✓✓✓✓✓✓✓✓✓✓✓✓✓✓✓✓INSTALLED✓✓✓✓✓✓✓
